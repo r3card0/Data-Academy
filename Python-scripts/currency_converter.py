fd3bf928c9318417a202c68f19b81138bc17f678
r3card0 <ricardo.rdz@pm.me>
2022-01-31 06:26:52 -0600

    Included mexican pesos to dollars convertion and dollars to mexican pesos convertion

--- Python-scripts/currency_converter.py
+++ Python-scripts/currency_converter.py
@@ -9,3 +9,25 @@ dollars = str(dollars)
 
 print("You have $" + dollars + " dollars")
 print("Thanks 😀 ")
+
+## convert from mexican pesos to dollars
+
+pesos = int(input("How many mexican pesos 💰 do you have?: "))
+pesos = float(pesos)
+dollar_price = 21.4
+dollars = round(pesos/dollar_price,2)
+dollars = str(dollars)
+
+print("You have $" + dollars + " dollars")
+print("Thanks 😀 ")
+
+## convert from dollars to mexican pesos
+
+dollars = int(input("How many dollars 💰 do you have?: "))
+dollars = float(dollars)
+dollar_price = 21.4
+pesos = round(dollars * dollar_price,2)
+pesos = str(pesos)
+
+print("You have $" + pesos + " mexican pesos")
+print("Thanks 😀 ")
