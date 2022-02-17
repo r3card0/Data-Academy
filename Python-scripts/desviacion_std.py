22b73572c51fe7e8c479fc5c2271fdc10d682b65
r3card0 <ricardo.rdz@pm.me>
2022-02-16 21:51:11 -0600

    returns the standard deviation of a list. fixed mean formula

--- Python-scripts/desviacion_std.py
+++ Python-scripts/desviacion_std.py
@@ -2,12 +2,14 @@
 from functools import reduce
 
 def std_dev(source_list:list):
-    mean = len(source_list) // 2
+    mean = (reduce(lambda r,e : r + e, source_list )) / len(source_list) 
     # list comprehension = [element for element in iterable if condicion]
     sigma_list = [((i - mean) ** 2) for i in source_list]
     # high order function: reduce
     sigma = reduce(lambda r,e: r + e, sigma_list)
-    dev_std = (sigma / (len(source_list) - 1)) ** 0.05
+    dev_std = (sigma / (len(source_list) - 1)) ** 0.5
     return print(round(dev_std,2))
 
-std_dev([65,100,54,112,351,4,56])
+std_dev([8,2,12,10])
+
+# [65,100,54,112,351,4,56]
